e1431b7f344329715d1ed231b9b98fd0a93d2718
Swarnendu Basak <36073547+OtakuKvothe@users.noreply.github.com>
2022-01-14 19:32:06 +0530

    Got a collectionview to work

--- App.js
+++ App.js
@@ -3,13 +3,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import ItemViewCard from './src/components/ItemViewCard';
+import ItemViewCollection from './src/components/ItemViewCollection';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ItemViewCard />
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ItemViewCollection />
     </View>
   );
 }
--- src/components/ItemViewCollection.js
+++ src/components/ItemViewCollection.js
@@ -1,1 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { AppRegistry, FlatList, StyleSheet, View } from 'react-native';
+import ItemViewCard from './ItemViewCard';
+
+class ItemViewCollection extends Component {
+    groupItems = (items, itemsPerRow) => {
+        // let itemsGroups = [];
+        // let group = [];
+        // items.forEach((item) => {
+        //     if (group.length === itemsPerRow) {
+        //         itemsGroups.push(group);
+        //         group = [item];
+        //     } else {
+        //         group.push(item);
+        //     }
+        // });
+
+        // if (group.length > 0) {
+        //     itemsGroups.push(group);
+        // }
+        let itemsx = [
+            [1, 2],
+            [3, 4],
+            [5, 6],
+            [7, 8],
+            [9, 10]
+        ];
+        //return itemsGroups;
+        return itemsx;
+    };
+
+    rendergroup = ({ item }) => {
+        const items = item.map((item, index) => {
+            return this.renderItem(item, index);
+        });
+        return (
+            <View style={styles.group}>
+                {items}
+            </View>
+        );
+    };
+
+    render = () => {
+        const groups = this.groupItems(this.props.items, this.props.itemsPerRow);
+        return (
+            <FlatList
+                {...this.props}
+                renderItem={this.rendergroup}
+                data={groups}
+            />
+        );
+    };
+
+    renderItem(item, index) {
+        return <ItemViewCard />
+    }
+}
+
+var styles = StyleSheet.create({
+    group: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        overflow: 'hidden'
+    }
+});
+
+export default ItemViewCollection;
