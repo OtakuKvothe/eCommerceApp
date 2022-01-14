20385b98f42aa08f3f21313171dfd780a952e29d
Swarnendu Basak <36073547+OtakuKvothe@users.noreply.github.com>
2022-01-14 15:34:40 +0530

    Redesign collection cell

--- src/components/ItemViewCard.js
+++ src/components/ItemViewCard.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import { Image, View, StyleSheet, Text } from 'react-native';
-import image from '../../static/res/react.svg';
 
 const styles = StyleSheet.create({
     container: {
         width: 180,
         height: 250,
-        elevation: 2,
+        elevation: 5,
         paddingLeft: 10,
         paddingRight: 10,
         paddingTop: 10,
         paddingBottom: 5,
-        justifyContent: 'center'
+        justifyContent: 'center',
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#fff',
+        backgroundColor: '#fff'
     },
     titleText: {
-        fontSize: 20
+        fontSize: 22
     },
     text: {
-        fontSize: 15
+        fontSize: 16
     },
     image: {
         width: 160,
-        height: 160
+        height: 160,
+        borderColor: "#000000",
+        borderWidth: 2,
+        borderRadius: 5
     }
 });
 
@@ -32,8 +38,8 @@ const ItemViewCard = () => {
             <View style={styles.container}>
                 <Image style={styles.image} source={require('../../static/res/react.svg')}>
                 </Image>
-                <Text style={styles.titleText}>Item Name</Text>
-                <Text style={styles.text}>Item Price</Text>
+                <Text style={styles.titleText}>React Logo</Text>
+                <Text style={styles.text}>₹400</Text>
             </View>
         </React.Fragment>
 
